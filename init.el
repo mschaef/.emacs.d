2f3ae16849eae33dc24b9d4d5595c4413971b2b4
schaem <schaem@pjm.com>
2012-05-22 15:55:17 +0100

    Add some basic commands for working with a personal log file.

--- init.el
+++ init.el
@@ -348,3 +348,34 @@ the current window."
 
 (global-set-key [(shift f4)] 'switch-to-scratch-buffer)
 
+;;;; Personal Log Support
+
+(push (cons "\\.orglog"  'org-mode) auto-mode-alist)
+
+(defun orglog-buffer-name-p (name)
+  "Determine if a buffer name is an orglog buffer. Returns the buffer
+name if it is, and nil otherwise."
+  (if (string-match "\\.orglog$" name)
+      name
+    nil))
+
+(defun orglog-buffer-names ()
+  "Return a list of all current orglog buffer names."
+  (delq nil
+        (mapcar #'orglog-buffer-name-p
+                (mapcar #'buffer-name (buffer-list)))))
+
+(defun orglog-most-recent-buffer-name ()
+  "Return the name of the orglog buffer corresponding to the most
+recent log file."
+  (car (reverse
+        (sort (orglog-buffer-names)
+              #'string<))))
+
+(defun orglog-switch-to-most-recent-buffer ()
+  "Make the most recent orglog buffer current and display it in
+the current window."
+  (interactive)
+  (switch-to-buffer (orglog-most-recent-buffer-name)))
+
+(global-set-key [f6] 'orglog-switch-to-most-recent-buffer)
