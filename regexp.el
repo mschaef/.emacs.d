981c284c36c5e873709d3f84c3bd580ce3f71b7a
schaem <schaem@pjm.com>
2012-05-30 15:14:53 +0100

    Add an interactive means of evaluating show-forward-regexp-matches.

--- regexp.el
+++ regexp.el
@@ -1,8 +1,10 @@
 
 (defun show-forward-regexp-matches (re)
+  (interactive "MRegexp: ")
   (save-excursion
     (re-search-forward re)
     (let ((ii 0))
       (while (stringp (match-string ii))
         (message "Match(%s): %s" ii (match-string ii))
         (incf ii)))))
+
