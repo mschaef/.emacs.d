07cc0059fd87ae39381befe22f52b2c0c436d1d0
schaem <schaem@pjm.com>
2013-03-06 18:03:12 +0000

    Change orglog into a minor mode.

--- orglog.el
+++ orglog.el
@@ -6,6 +6,19 @@
 (defvar orglog-root "~/.emacs/orglog"
   "Root directory for orglog files.")
 
+(defvar orglog-header-format-string "%Y-%m-%d"
+  "Format string for orglog header entries. Must be compatible
+with `format-time-string'.")
+
+(defvar orglog-file-basename-format-string "%Y-%m"
+  "Format string for orglog file base names. Must be compatible
+with `format-time-string'.")
+
+(define-minor-mode orglog-mode
+  "Toggle interpretation of a buffer as an orglog buffer."
+  :lighter " Orglog"
+  :keymap '(([(shift f6)] . orglog-enter-day)))
+
 (defun orglog-find-root ()
   (if (not (file-exists-p orglog-root))
       (progn
@@ -18,10 +31,10 @@
       orglog-root)))
 
 (defun orglog-today-header ()
-  (format-time-string "%Y-%m-%d" (current-time)))
+  (format-time-string orglog-header-format-string (current-time)))
 
 (defun orglog-today-basename ()
-  (format-time-string "%Y-%m" (current-time)))
+  (format-time-string orglog-file-basename-format-string (current-time)))
 
 (defun orglog-todays-file-name ()
   (concat (orglog-find-root) "/" (orglog-today-basename) ".orglog"))
@@ -44,10 +57,13 @@
 
 (global-set-key [f6] 'orglog-find-todays-file)
 
-(defun orglog-turn-on-bindings ()
-  (local-set-key [(shift f6)] 'orglog-enter-day))
+(defun orglog-activate-for-orglog-buffers ()
+  (when (and buffer-file-truename
+             (string-suffix-p buffer-file-truename
+                              ".orglog"))
+    (orglog-mode)))
 
-(add-hook 'org-mode-hook 'orglog-turn-on-bindings)
+(add-hook 'org-mode-hook 'orglog-activate-for-orglog-buffers)
 
 
 (push (cons "\\.orglog" 'org-mode) auto-mode-alist)
