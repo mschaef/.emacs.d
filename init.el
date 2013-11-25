0496464a1b45a41235c7ce02dc9590c944b5ef9f
Mike Schaeffer <mschaeffer@ksmpartners.com>
2013-11-25 10:27:14 -0500

    Add ClojureScript files to the auto-mode-alist

--- init.el
+++ init.el
@@ -180,6 +180,7 @@
 (autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
 
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
 
 ;;; paredit
 
