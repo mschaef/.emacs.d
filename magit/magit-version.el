d1787f47145ef931a0e2f3c49da6cb131f120fe0
Mike Schaeffer <mschaeffer@ksmpartners.com>
2014-03-05 13:28:31 -0500

    Add magit.

---
+++ magit/magit-version.el
@@ -0,0 +1,13 @@
+;;; magit-version.el --- the Magit version you are using
+
+(setq magit-version "1.2.0-2228-g5a0580e")
+
+(provide 'magit-version)
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; magit-version.el ends here
