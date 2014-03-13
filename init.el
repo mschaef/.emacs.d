27aa20a90b6a4ee30bf9bf61f18a10cb4bbe6cf2
Mike Schaeffer <mschaeffer@ksmpartners.com>
2014-03-13 15:16:39 -0400

    Remove the C-w rebinding to backward-kill-word.

--- init.el
+++ init.el
@@ -169,11 +169,6 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
-;; Use C-w to kill the previous word and rebind kill-region to something else.
-
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\C-c\C-k" 'kill-region)
 
 ;;;;; Load cygwin32-mount on Windows
 
