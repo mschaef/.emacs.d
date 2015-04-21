7987d1b57d983413db02596fad76c261ecfdceb1
Mike Schaeffer <mike@mschaef.com>
2015-04-20 23:02:59 -0400

    Move uniq-lines down into lisp/.

--- init.el
+++ init.el
@@ -227,27 +227,6 @@
                           "|" "DONE" "NOT-DONE")
       org-todo-interpretation 'sequence)
 
-;;;; Remove duplicate lines
-
-(defun uniq-lines (beg end)
-  "Unique lines in region.
-Called from a program, there are two arguments:
-BEG and END (region to sort)."
-  (interactive "r")
-  (save-excursion
-    (save-restriction
-      (narrow-to-region beg end)
-      (goto-char (point-min))
-      (while (not (eobp))
-        (kill-line 1)
-        (yank)
-        (let ((next-line (point)))
-          (while
-              (re-search-forward
-               (format "^%s" (regexp-quote (car kill-ring))) nil t)
-            (replace-match "" nil nil))
-          (goto-char next-line))))))
-
 ;;;; Start the emacs server
 
 (when window-system
--- lisp/lisp-utilities.el
+++ lisp/lisp-utilities.el
@@ -152,5 +152,26 @@ arguments are written in princ format."
       (setq current-directory-list (cdr current-directory-list)))
     files-list))
 
+;;;; Remove duplicate lines
+
+(defun uniq-lines (beg end)
+  "Unique lines in region.
+Called from a program, there are two arguments:
+BEG and END (region to sort)."
+  (interactive "r")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region beg end)
+      (goto-char (point-min))
+      (while (not (eobp))
+        (kill-line 1)
+        (yank)
+        (let ((next-line (point)))
+          (while
+              (re-search-forward
+               (format "^%s" (regexp-quote (car kill-ring))) nil t)
+            (replace-match "" nil nil))
+          (goto-char next-line))))))
+
 (provide 'lisp-utilities)
 
