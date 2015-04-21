04452ef7aea3fc3823bc32256650068e67b1e200
Mike Schaeffer <mike@mschaef.com>
2015-04-20 23:26:40 -0400

    Push nREPL config to after the package system starts up.

--- init.el
+++ init.el
@@ -342,19 +342,6 @@ the current fill-column."
  ;; If there is more than one, they won't work right.
  )
 
-;;;; Configure Cider and nREPL
-
-(setq nrepl-hide-special-buffers t)
-(setq nrepl-port "53095")
-
-(setq cider-repl-print-length 10) 
-
-;; disable to hopefully fix hang
-;;(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-(setq cider-popup-stacktraces nil)
-(setq cider-repl-popup-stacktraces t)
-
-
 ;;;; Customize uniquify to get more rational unique buffer names
 
 (setq uniquify-buffer-name-style 'post-forward)
--- lisp/package-init.el
+++ lisp/package-init.el
@@ -3,3 +3,16 @@
 ;;;; Post-package initialization
 
 
+;;;; Configure Cider and nREPL
+
+(setq nrepl-hide-special-buffers t)
+(setq nrepl-port 53095)
+
+(setq cider-repl-print-length 10) 
+
+;; disable to hopefully fix hang
+;;(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(setq cider-popup-stacktraces nil)
+(setq cider-repl-popup-stacktraces t)
+
+
