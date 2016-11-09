baaa07a97f02e97a953dfae914910fffa55c1afa
Mike Schaeffer <michael.schaeffer@lab49.com>
2016-11-09 11:19:18 -0500

    Adjust some default paramaters to better deal with Java projects.

--- init.el
+++ init.el
@@ -320,8 +320,8 @@ the current fill-column."
 
 (setq ffip-patterns
       (append '("*.c"  "*.cc"  "*.clj"  "*.coffee"  "*.cs" "*.css"
-                "*.el"  "*.ftl" "*.h" "*.html" "*.java" "*.js"
-                "*.json" "*.scm" "*.scss" "*.sh" "*.xml")
+                "*.csv" "*.el"  "*.ftl" "*.h" "*.html" "*.java" "*.js"
+                "*.json" "*.scm" "*.scss" "*.sh" "*.sql" "*.xml")
        ffip-patterns))
 
 (global-set-key (kbd "C-x f") 'find-file-in-project)
@@ -340,6 +340,9 @@ the current fill-column."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(develock-max-column-plist
+   (quote
+    (emacs-lisp-mode 79 lisp-interaction-mode w change-log-mode t texinfo-mode t c-mode 79 c++-mode 79 java-mode 120 jde-mode 79 html-mode 79 html-helper-mode 79 cperl-mode 79 perl-mode 79 mail-mode t message-mode t cmail-mail-mode t tcl-mode 79 ruby-mode 79)))
  '(inhibit-startup-echo-area-message "mschaef")
  '(inhibit-startup-screen t)
  '(initial-buffer-choice t)
@@ -356,7 +359,8 @@ the current fill-column."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
+ '(develock-long-line-2 ((t (:background "gray50"))))
+ '(develock-whitespace-2 ((t (:background "DarkOrange4"))))
  '(org-level-1 ((t (:inherit outline-1 :foreground "yellow" :background "gray50"))))
  '(org-level-2 ((t (:inherit outline-2 :foreground "gray80"))))
  '(org-level-3 ((t (:inherit outline-3 :foreground "gray75"))))
