4d9033abc178f15f982de1b7ea44d6a9b84ff5bd
Mike Schaeffer <michael.schaeffer@lab49.com>
2018-03-27 22:49:49 -0400

    Add magit-find-file.

--- init.el
+++ init.el
@@ -355,7 +355,7 @@ the current fill-column."
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (magit ## typescript-mode yasnippet paredit markdown-mode js2-mode cider)))
+    (magit-find-file ## typescript-mode yasnippet paredit markdown-mode js2-mode cider)))
  '(safe-local-variable-values (quote ((sh-indent-comment . t) (lexical-binding . t))))
  '(tool-bar-mode nil))
 
