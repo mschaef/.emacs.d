c73d61f0c9b775e0807469dd4536899d322d05d5
Mike Schaeffer <mike.schaeffer@digitalasset.com>
2021-01-05 06:44:29 -0500

    Add instructions for updating packages in .emacs.d

--- update-archived-packages.sh
+++ update-archived-packages.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Instructions for end-to-end package upgrade:
+#
+# 1) M-x list-packages
+# 2) U (package-menu-mark-upgrades)
+# 3) x (package-menu-execute)
+# 4) ./update-archived-packages.sh in .emacs.d/
+
 git status | grep deleted | cut -d: -f2 | cut -d/ -f-2 | sort | uniq | xargs git rm -r
 
 git status | grep -v deleted | grep elpa | xargs git add
