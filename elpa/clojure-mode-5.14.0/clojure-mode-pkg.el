8cdb6ed1a0e5b0c7e96740ab5e96ecb5923d40d1
Mike Schaeffer <mike@mschaef.com>
2022-05-28 11:48:52 -0400

    Upgrade packages.

---
+++ elpa/clojure-mode-5.14.0/clojure-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from clojure-mode.el  -*- no-byte-compile: t -*-
+(define-package "clojure-mode" "5.14.0" "Major mode for Clojure code" '((emacs "25.1")) :commit "b7d08b87f6a116ff47b33ee857926b60c66c3ab7" :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.dev") :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
