c881fc46586c45d248a0746c029898e89d6a32ca
Mike Schaeffer <mike@mschaef.com>
2023-06-12 07:08:58 -0400

    Add a facility for invoking ack in the current directory.

--- lisp/project-ack.el
+++ lisp/project-ack.el
@@ -9,24 +9,46 @@
 
 (require 'find-file-in-project)
 
-(defun project-ack (pattern project-root)
-  "Run ack, with user-specified ARGS, and collect output in a buffer.
-While ack runs asynchronously, you can use the \\[next-error]
-command to find the text that ack hits refer to. The command
-actually run is defined by the ack-command variable. By default,
-this function guesses the item to search for based on the symbol at
-point. This can be disabled with a prefix argument."
+(defun project-ack (pattern ack-search-root-dir)
+  "Runs ack at the project root, with user-specified ARGS, and
+collect output in a buffer.  While ack runs asynchronously, you
+can use the \\[next-error] command to find the text that ack hits
+refer to. The command actually run is defined by the ack-command
+variable. By default, this function guesses the item to search
+for based on the symbol at point. This can be disabled with a
+prefix argument."
   (interactive
-   (let ((project-root (ffip-get-project-root-directory)))
+   (let ((ack-search-root-dir (ffip-get-project-root-directory)))
      (list
-      (read-string (concat "Project Ack (in " project-root "): ")
+      (read-string (concat "Project Ack (in " ack-search-root-dir "): ")
                    (concat ack-command
                            (if current-prefix-arg
                                ""
                              (thing-at-point 'symbol))))
-      project-root)))
-  (ack pattern project-root))
+      ack-search-root-dir)))
+  (ack pattern ack-search-root-dir))
 
+(defun current-ack (pattern ack-search-root-dir)
+  "Runs ack at the default directory, with user-specified ARGS, and
+collect output in a buffer.  While ack runs asynchronously, you
+can use the \\[next-error] command to find the text that ack hits
+refer to. The command actually run is defined by the ack-command
+variable. By default, this function guesses the item to search
+for based on the symbol at point. This can be disabled with a
+prefix argument."
+  (interactive
+   (let ((ack-search-root-dir default-directory))
+     (list
+      (read-string (concat "Ack (in current directory: " ack-search-root-dir "): ")
+                   (concat ack-command
+                           (if current-prefix-arg
+                               ""
+                             (thing-at-point 'symbol))))
+      ack-search-root-dir)))
+  (ack pattern ack-search-root-dir))
+
+(global-set-key [(control f7)] 'current-ack)
 (global-set-key [(control shift f7)] 'project-ack)
 
 (provide 'project-ack)
+
