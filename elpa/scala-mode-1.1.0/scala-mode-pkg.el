d118db5ff30db84ed377792708ec413e3121d73d
Mike Schaeffer <mike@mschaef.com>
2024-11-27 17:27:16 -0500

    Upgrade packages.

--- elpa/scala-mode-1.1.0/scala-mode-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "scala-mode" "1.1.0" "Major mode for editing Scala" 'nil :commit "46bb948345f165ebffe6ff3116e36a3b8a3f219d" :keywords
-  '("languages")
-  :url "https://github.com/hvesalai/emacs-scala-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
