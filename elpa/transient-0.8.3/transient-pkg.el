1d48ee0a5e84494c8c754f26630df35672005ce8
Mike Schaeffer <mike@mschaef.com>
2025-01-31 11:55:23 -0500

    Upgrade packages.

---
+++ elpa/transient-0.8.3/transient-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "transient" "0.8.3"
+  "Transient commands."
+  '((emacs  "26.1")
+    (compat "30.0.0.0")
+    (seq    "2.24"))
+  :url "https://github.com/magit/transient"
+  :commit "000ff15942878aa1108abaa020da86ada675fea9"
+  :revdesc "v0.8.3-0-g000ff1594287"
+  :keywords '("extensions")
+  :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))
