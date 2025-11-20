b8377184d5d4440f5a82a056b2dec975a04194c9
Mike Schaeffer <mike@mschaef.com>
2025-11-20 06:53:00 -0500

    Upgrade packages.

--- elpa/clojure-mode-5.19.0/clojure-mode-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "clojure-mode" "5.19.0"
-  "Major mode for Clojure code."
-  '((emacs "25.1"))
-  :url "https://github.com/clojure-emacs/clojure-mode"
-  :commit "4afdd3539036bbd6b1c01b2e00559676c4d40085"
-  :revdesc "v5.19.0-0-g4afdd3539036"
-  :keywords '("languages" "clojure" "clojurescript" "lisp")
-  :maintainers '(("Bozhidar Batsov" . "bozhidar@batsov.dev")))
---
+++ elpa/dash-2.20.0/dash-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "dash" "2.20.0"
+  "A modern list library for Emacs."
+  '((emacs "24"))
+  :url "https://github.com/magnars/dash.el"
+  :commit "fcb5d831fc08a43f984242c7509870f30983c27c"
+  :revdesc "2.20.0-0-gfcb5d831fc08"
+  :keywords '("extensions" "lisp")
+  :authors '(("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainers '(("Basil L. Contovounesios" . "basil@contovou.net")))
