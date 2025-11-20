0ba584adb033ad02ac487d53f7553778d14de6bb
Mike Schaeffer <mike@mschaef.com>
2025-11-20 06:55:32 -0500

    Add Haskell and Scala modes.

---
+++ elpa/haskell-mode-17.5/haskell-mode-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "haskell-mode" "17.5"
+  "A Haskell editing mode."
+  '((emacs "25.1"))
+  :url "https://github.com/haskell/haskell-mode"
+  :commit "8d0f44bfe2a9ab6b0969c9bafb75089f315ff5ae"
+  :revdesc "8d0f44bfe2a9"
+  :keywords '("haskell" "cabal" "ghc" "repl" "languages"))
---
+++ elpa/scala-mode-1.1.0/scala-mode-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "scala-mode" "1.1.0"
+  "Major mode for editing Scala."
+  ()
+  :url "https://github.com/hvesalai/emacs-scala-mode"
+  :commit "46bb948345f165ebffe6ff3116e36a3b8a3f219d"
+  :revdesc "46bb948345f1"
+  :keywords '("languages"))
