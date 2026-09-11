34437feccf9e102e2b07e1ace2eea2a533455f42
Mike Schaeffer <mike@mschaef.com>
2026-09-11 12:47:43 -0400

    Upgrade packages.

---
+++ elpa/clojure-mode-5.23.0/clojure-mode-pkg.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "clojure-mode" "5.23.0"
+  "Major mode for Clojure code."
+  '((emacs "27.1"))
+  :url "https://github.com/clojure-emacs/clojure-mode"
+  :commit "9146525680b29a0c6b72a1126d075b54f8066710"
+  :revdesc "9146525680b2"
+  :keywords '("languages" "clojure" "clojurescript" "lisp")
+  :authors '(("Jeffrey Chu" . "jochu0@gmail.com")
+             ("Lennart Staflin" . "lenst@lysator.liu.se")
+             ("Phil Hagelberg" . "technomancy@gmail.com")
+             ("Bozhidar Batsov" . "bozhidar@batsov.dev")
+             ("Artur Malabarba" . "bruce.connor.am@gmail.com")
+             ("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainers '(("Bozhidar Batsov" . "bozhidar@batsov.dev")))
---
+++ elpa/scala-mode-1.1.1/scala-mode-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from scala-mode.el  -*- no-byte-compile: t -*-
+(define-package "scala-mode" "1.1.1" "Major mode for editing Scala" '((emacs "25.1")) :commit "df60066e3194428266b0c66c7619327e6a4bf48d" :keywords '("languages") :url "https://github.com/hvesalai/emacs-scala-mode")
